e0d59a57bb17a8cb285dfd749df348f8ac08373c
eemektas <eemektas@gmail.com>
2020-02-24 14:10:13 +0300

    test

---
+++ quests/repLetter.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import timeit
+import sys
+
+
+def repLetter(string):
+	for l in string:
+		if string.count(l) > 1:
+			return l
+
+string = 'Intergalactic'
+print('string: ', string)
+result = repLetter(string)
+print('1st repeated letter: ', result)
+
+# Time complexity:
+tcode = """
+def repCount(string):
+	for l in string:
+		if string.count(l) > 1:
+			return l
+
+string = 'Intergalactic'
+repCount(string)
+"""
+
+print('min time: ', min(timeit.repeat(stmt=tcode, repeat=10)), 'second')
+print('max time: ', max(timeit.repeat(stmt=tcode, repeat=10)), 'second')
+
+# Space complexity:
+print('space usage: ', sys.getsizeof(result), 'bytes')
