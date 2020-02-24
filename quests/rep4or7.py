2f3e7b3186daf0363f13745a9c4da6e1c762c168
eemektas <eemektas@gmail.com>
2020-02-24 17:51:31 +0300

    final

--- quests/rep4or7.py
+++ quests/rep4or7.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 
+import time
 import timeit
 import sys
 
@@ -15,10 +16,12 @@ def arr4or7(arr):
 	print(arr)
 	return 1
 
-
 arr = [i for i in range(1, 101)]
-result = print(arr4or7(arr))
+result = arr4or7(arr)
+
+t1 = time.clock()
 result
+t2 = time.clock()
 
 # Time complexity:
 tcode = """
@@ -36,9 +39,10 @@ arr = [i for i in range(1, 101)]
 arr4or7(arr)
 """
 
-time_comp = timeit.repeat(stmt=tcode, repeat=10)
+time_comp = timeit.repeat(stmt=tcode, repeat=2)
 print('min time: ', min(time_comp), 'second')
 print('max time: ', max(time_comp), 'second')
+print('actual duration: ', t2 - t1, 'sec')
 
 # Space complexity:
 print('space usage: ', sys.getsizeof(result) + sys.getsizeof(arr), 'bytes')
--- quests/repLetter.py
+++ quests/repLetter.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 
+import time
 import timeit
 import sys
 
@@ -12,22 +13,26 @@ def repLetter(string):
 string = 'Intergalactic'
 print('string: ', string)
 result = repLetter(string)
+t1 = time.clock()
+result
+t2 = time.clock()
 print('1st repeated letter: ', result)
 
 # Time complexity:
 tcode = """
-def repCount(string):
+def repLetter(string):
 	for l in string:
 		if string.count(l) > 1:
 			return l
 
 string = 'Intergalactic'
-repCount(string)
+repLetter(string)
 """
 
 time_comp = timeit.repeat(stmt=tcode, repeat=10)
 print('min time: ', min(time_comp), 'second')
 print('max time: ', max(time_comp), 'second')
+print('actual duration: ', t2 - t1, 'second')
 
 # Space complexity:
 print('space usage: ', sys.getsizeof(result) + sys.getsizeof(string), 'bytes')
